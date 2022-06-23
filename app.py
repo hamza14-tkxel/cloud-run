f7398d357164d585e6f4cb546555753f90441bc6
Hamza Javed <87646100+hamza14-tkxel@users.noreply.github.com>
2022-06-23 19:44:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
-  environment = str(os.environ.get('ENVIRONMENT'))
-  return "Hello World!\n" + environment
+  environment = str(os.environ)
+  return environment
 
 
 if __name__ == '__main__':
